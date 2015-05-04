ef4381c4267a02a82a7954713e78c726dc37268b
nachoegido <nachoegido@gmail.com>
2015-05-04 10:31:20 +0200

    Primera pregunta y enlace a GITHUB

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
+
 var app = express();
 
 // view engine setup
@@ -19,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 
 app.use('/', routes);
 
