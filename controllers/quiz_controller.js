981b470cc4cfb998c10dd95c5e2a9a6a80f9044b
nachoegido <nachoegido@gmail.com>
2015-05-08 11:46:16 +0200

    Busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,28 +1,57 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el codigo su ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new Error('No existe quizId=' + quizId)); }
+		}
+	).catch(function(error) { next(error);});
+};
+
+// GET /quizes?search=texto_a_buscar
+exports.search = function(req, res) {
+	var busqueda= req.query.busqueda || '';
+  	var busqueda_like="%"+busqueda.replace(/ +/g, "%")+"%";
+	models.Quiz
+	.findAll({where: ["pregunta like ?", busqueda_like, busqueda_like],
+		order: 'updatedAt DESC'
+		})
+	.then(function(quizes){
+		res.render('quizes/search',{
+			quizes: quizes
+		});
+	})
+	.error(function(error){
+		console.log("Error: No se puede buscar en los quizes.");
+		res.redirect('/');
+	});
+
+}
+
 // GET /quizes
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', { quizes: quizes});
+		res.render('quizes/index', { quizes: quizes});
 	})
+	.catch(function(error) { next(error);});
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', { quiz: quiz});
-	})
+	res.render('quizes/show', { quiz: req.quiz});
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		if (req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer',
-				{ quiz: quiz, respuesta: 'Correcto'});
-		} else {
-			res.render('quizes/answer',
-				{ quiz: quiz, respuesta: 'Incorrecto'});
-		}
-	})
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta){
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer',
+			{ quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,14 @@ router.get('/author', function(req, res) {
   res.render('author', {  });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definicion de rutas de /quizes
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+router.get('/quizes/search', 				quizController.search);
 /*router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 */
