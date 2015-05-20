8da4fd687e0951419dec39bc233af48b10355bff
nachoegido <nachoegido@gmail.com>
2015-05-20 10:34:29 +0200

    Crear comentarios

--- models/models.js
+++ models/models.js
@@ -30,7 +30,15 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
+//Importar la definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);  //indica que los comentarios pertenecen a los quizes
+Quiz.hasMany(Comment);	//indica que un quiz puede tener varios comentarios
+
 exports.Quiz = Quiz; //exportar definicion de tabla Quiz
+exports.Comment = Comment;
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
